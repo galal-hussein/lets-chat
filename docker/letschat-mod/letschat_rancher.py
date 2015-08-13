8db88b2c0cdfcafb7a7651adcb0866e49992ac4d
Hussein Galal <hussein.galal.ahmed.11@gmail.com>
2015-08-12 20:16:28 -0400

    change the DNS library

--- docker/letschat-mod/letschat_rancher.py
+++ docker/letschat-mod/letschat_rancher.py
@@ -1,15 +1,10 @@
 import os
 import socket
-import dns.resolver
+import DNS
 
 def fetchcont(service_name):
-    arecords = dns.resolver.query(service_name,'A')
-    ret_txt = ""
-    for i in range(len(arecords)):
-        if i == len(arecords)-1:
-            ret_txt = ret_txt+str(arecords[i])+":27017"
-        else:
-            ret_txt = ret_txt+str(arecords[i])+":27017,"
+    query = DNS.dnslookup(service_name,'A') 
+    ret_txt = ":27017,".join(query)+":27017"
     print ret_txt
 
 if __name__ == "__main__":
